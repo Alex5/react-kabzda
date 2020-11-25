f2ccc56bdab858b5175c22e99172ba9b4537f52a
Alex5 <angrymarshall@gmail.com>
2020-11-25 18:12:27 +0300

    CI: ""

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -18,31 +18,40 @@ const ProfileInfo = (props) => {
                 title={props.profile.fullName}
                 subTitle=""
             />
-            <div className={styles.profile__body}>
+            <div className={styles.profile__content}>
                 <div className={styles.profile__avatar}>
                     <img
                         src={props.profile.photos.large}
                         alt="Jon Snow"/>
                 </div>
-                <div className={styles.profile__body__info}>
-                    <div>
-                        <h3>Description:</h3>
-                        <p>{props.profile.aboutMe}</p>
+                <div className={styles.profile__content_body}>
+                    <div className={styles.profile__body__info}>
+                        <div><h3>Description:</h3></div>
+                        <div><p>{props.profile.aboutMe}</p></div>
                     </div>
-                    <div>
-                        <h3>Contacts:</h3>
-                        <div><a href={props.profile.contacts.facebook}></a></div>
-                        <div>{props.profile.contacts.website}</div>
-                        <div><a href={props.profile.contacts.vk}>{props.profile.contacts.vk}</a></div>
-                        <div>{props.profile.contacts.twitter}</div>
-                        <div>{props.profile.contacts.instagram}</div>
-                        <div>{props.profile.contacts.youtube}</div>
-                        <div>{props.profile.contacts.github}</div>
-                        <div>{props.profile.contacts.mainLink}</div>
+                    <div className={styles.profile__body__info}>
+                        <div><h3>Contacts:</h3></div>
+                        <div>
+                            <div><a href={props.profile.contacts.facebook}></a></div>
+                            <div>{props.profile.contacts.website}</div>
+                            <div><a href={props.profile.contacts.vk}>{props.profile.contacts.vk}</a></div>
+                            <div>{props.profile.contacts.twitter}</div>
+                            <div>{props.profile.contacts.instagram}</div>
+                            <div>{props.profile.contacts.youtube}</div>
+                            <div>{props.profile.contacts.github}</div>
+                            <div>{props.profile.contacts.mainLink}</div>
+                        </div>
+
                     </div>
-                    <div><h3>Looking for a job:</h3> {props.profile.lookingForAJob ? `Yes` : `No`}</div>
-                    <div><h3>Job
-                        Description:</h3> {props.profile.lookingForAJobDescription ? props.profile.lookingForAJobDescription : `No job description`}
+                    <div className={styles.profile__body__info}>
+                        <div><h3>Looking for a job:</h3></div>
+                        <div>{props.profile.lookingForAJob ? `Yes` : `No`}</div>
+                    </div>
+                    <div className={styles.profile__body__info}>
+                        <div><h3>Job
+                            Description:</h3>
+                        </div>
+                        <div>{props.profile.lookingForAJobDescription ? props.profile.lookingForAJobDescription : `No job description`}</div>
                     </div>
                 </div>
             </div>
